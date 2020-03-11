2776d707da708a0a753d8b4bfe6d9a6510ec8f02
Audaci <492848019@qq.com>
2020-03-11 08:31:48 +0800

    1

--- src/views/settings/smartTips.vue
+++ src/views/settings/smartTips.vue
@@ -1,7 +1,8 @@
 <template>
   <div>
     <div class="header">
-      <el-button size="small" @click="addSmarttips()">新增</el-button>
+      <el-button type="primary" icon="el-icon-edit"  size="middle" @click="addSmarttips">添加智能提示</el-button>
+      
     </div>
 
     <div class="content">
@@ -10,12 +11,12 @@
           <template slot-scope="scope">{{scope.$index+1}}</template>
         </el-table-column>
         <el-table-column prop="preReg" label="正则表达式" width="200"></el-table-column>
-        <el-table-column prop="nextWord" label="提示词" width="200"></el-table-column>
+        <el-table-column prop="nextWord" label="提示词" width="500"></el-table-column>
 
-        <el-table-column label="操作" width="150">
+        <el-table-column label="操作" width="250">
           <template>
-            <el-button size="small" @click="addSmarttips()">新增</el-button>
-            <el-button size="small" @click="addSmarttips()">新增</el-button>
+            <el-button type="success" icon="el-icon-edit" size="middle" @click="updateSmarttips()">修改</el-button>
+            <el-button  type="danger" icon="el-icon-delete" size="middle" @click="deleteSmarttips(userinfo)">删除</el-button>
           </template>
         </el-table-column>
       </el-table>
@@ -24,7 +25,7 @@
 </template>
 
 <script>
-import { getSmartTipsList } from "@/api/api";
+import { getSmartTipsList , deleteSmarttips} from "@/api/api";
 
 export default {
   data() {
@@ -47,12 +48,44 @@ export default {
           this.list = data;
         }
       });
-    }
+    },
+    addSmarttips() {
+      
+     
+    },
+    updateSmarttips() {
+
+    },
+    deleteSmarttips() {
+        this.$confirm("是否删除此条记录?", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      })
+        .then(() => {
+          let params = { oper: this.userinfo.userId };
+         deleteSmarttips(params).then(res => {
+            let { flag, data, errMsg } = res;
+            if (!flag) {
+              this.$message({
+                message: errMsg,
+                type: "error"
+              });
+            } else {
+              this.list = data;
+            }
+          });
+        })
+        .catch(() => {
+          this.$message({
+            type: "info",
+            message: "已取消删除"
+          });
+        });
+    },
   },
 
-  addSmarttips() {},
-  deleteSmarttips() {},
-  updateSmarttips() {},
+  
 
   components: {},
   mounted() {
